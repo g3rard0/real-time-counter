800d168d6be23aae54dc81471e6ecbf01d397c86
Gerardo Venegas <venegasgl@live.com>
2019-11-09 02:34:13 -0800

    Updates

---
+++ precache-manifest.e169cbfbcefbbc91c0a6fbfc7385c5f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410a58f46e6d9426ae4e1e8e3458f729",
+    "url": "/real-time-counter/index.html"
+  },
+  {
+    "revision": "6cadb39418582b847112",
+    "url": "/real-time-counter/static/css/main.612f4d47.chunk.css"
+  },
+  {
+    "revision": "955a8d4f81c1b75ff5fd",
+    "url": "/real-time-counter/static/js/2.b730bfe0.chunk.js"
+  },
+  {
+    "revision": "6cadb39418582b847112",
+    "url": "/real-time-counter/static/js/main.75a3e150.chunk.js"
+  },
+  {
+    "revision": "75c6c4da37ed9eaba9ab",
+    "url": "/real-time-counter/static/js/runtime-main.3f1e2f5c.js"
+  }
+]);
